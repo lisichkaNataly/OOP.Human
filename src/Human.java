32d291b302bc0c516e2cd7f57e733dd4ba01610d
Izmaylova <Natalyatf@yandex.ru>
2022-10-24 18:01:17 +0500

    ООП Часть 1 Домашнее задание 1,2

---
+++ src/Human.java
@@ -0,0 +1,18 @@
+public class Human {
+
+    int yearOfBirth;
+    String name;
+    String town;
+    String post;
+
+
+    Human(String name, String town, int yearOfBirth, String post) {
+        this.name = name;
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+        this.post = post;
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        Human human = new Human("Максим", "Минск", 1987, "бренд-менеджер");
+        Human human1 = new Human("Аня", "Москва", 1993, "методист образовательных программ");
+        Human human2 = new Human("Катя", "Калининград", 1994, "продакт-менеджер");
+        Human human3 = new Human("Артем", "Москва", 1995, "директор по развитию бизнеса");
+
+        System.out.println("Привет!Меня зовут "+ human.name + "." + " Я из города - " + human.town + "." +
+                " Я родился в " + human.yearOfBirth + " году." + " Я работаю на должности: " + human.post + "." +
+                "Будем знакомы!");
+        System.out.println("Привет!Меня зовут "+ human1.name + "." + " Я из города - " + human1.town + "." +
+                " Я родился в " + human1.yearOfBirth + " году." + " Я работаю на должности: " + human1.post + "." +
+                "Будем знакомы!");
+        System.out.println("Привет!Меня зовут "+ human2.name + "." + " Я из города - " + human2.town + "." +
+                " Я родился в " + human2.yearOfBirth + " году." + " Я работаю на должности: " + human2.post + "." +
+                "Будем знакомы!");
+        System.out.println("Привет!Меня зовут "+ human3.name + "." + " Я из города - " + human3.town + "." +
+                " Я родился в " + human3.yearOfBirth + " году." + " Я работаю на должности: " + human3.post + "." +
+                "Будем знакомы!");
 
     }
 }
